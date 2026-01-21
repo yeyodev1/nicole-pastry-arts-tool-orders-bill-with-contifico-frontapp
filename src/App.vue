af6ac79c321a0250ff2ae8aa373b6a732220d36f
Diego Reyes <diegorele13@gmail.com>
2026-01-21 15:37:27 -0500

    feat(navigation): add ProductionNavBar for production role routes
    Conditionally render ProductionNavBar when route meta has production role, otherwise fall back to regular NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { RouterView, useRoute } from 'vue-router'
 import NavBar from './components/NavBar.vue'
+import ProductionNavBar from './components/ProductionNavBar.vue'
 
 const route = useRoute()
 </script>
 
 <template>
   <div class="app-layout">
-    <NavBar v-if="route.name !== 'login'" />
+    <ProductionNavBar v-if="route.meta.role === 'production'" />
+    <NavBar v-else-if="route.name !== 'login'" />
     <RouterView />
   </div>
 </template>
